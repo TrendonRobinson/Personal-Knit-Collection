ad64cab116cc4e6bc13d8a570bfdd6f49b9877c0
Trendon Robinson <46141975+TrendonRobinson@users.noreply.github.com>
2022-09-16 01:34:46 -0400

    Fixing main template

---
+++ src/ReplicatedStorage/Controllers/TestController.lua
@@ -0,0 +1,28 @@
+--// Services
+
+--// Modules
+
+--// Knit
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+--// Controller
+local TestController = Knit.CreateController { Name = "TestController" }
+
+--// Variables
+function TestController:KnitInit()
+    
+end
+
+function TestController:KnitStart()
+    -------------Variables-----------
+    
+    -------------Variables-----------
+    -------------Classes-------------
+    
+    -------------Classes-------------
+    -----------Initialize------------
+    
+    -----------Initialize------------
+end
+
+return TestController
---
+++ src/ServerScriptService/Services/TestService.lua
@@ -0,0 +1,30 @@
+--// Services
+
+--// Modules
+
+--// Knit
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+--// Service
+local TestService = Knit.CreateService {
+    Name = "TestService";
+    Client = {};
+}
+
+function TestService:KnitInit()
+    
+end
+
+function TestService:KnitStart()
+    -------------Variables-----------
+    
+    -------------Variables-----------
+    -------------Classes-------------
+    
+    -------------Classes-------------
+    -----------Initialize------------
+    
+    -----------Initialize------------
+end
+
+return TestService
